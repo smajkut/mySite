df1416b5a4bbb2a88d55feb2f21f28208c896bca
Slawomir <smajkut94@gmail.com>
2022-11-03 23:27:38 +0100

    Working at JS to navigation. Finished nav list and nav btn.

---
+++ script.js
@@ -0,0 +1,12 @@
+const navBtn = document.querySelector('.burger-btn')
+const navArrow = document.querySelector('.fa-angle-left')
+const navList = document.querySelector('.navigation-list')
+
+
+const showNav = () => {
+    navArrow.classList.toggle('btn-rotate')
+    navList.classList.toggle('show-navigation-list')
+
+}
+
+navBtn.addEventListener('click', showNav)
