4ea77171ade46d6de9420a1ee0c869d6020d14ae
Slawomir <smajkut94@gmail.com>
2022-11-06 22:10:51 +0100

    Update

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const navBtn = document.querySelector('.burger-btn')
 const navArrow = document.querySelector('.fa-angle-left')
 const navList = document.querySelector('.navigation-list')
+const navHead = document.querySelector('#header')
 
 
 const showNav = () => {
     navBtn.classList.toggle('btn-rotate')
     navList.classList.toggle('show-navigation-list')
+    navHead.classList.toggle('header-scale')
 
 }
 navBtn.addEventListener('click', showNav)
