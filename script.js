7b956e4bf987b22567df4b8ced521c79a092876c
Slawomir <smajkut94@gmail.com>
2022-11-07 22:34:01 +0100

    Update

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ const navArrow = document.querySelector('.fa-angle-left')
 const navList = document.querySelector('.navigation-list')
 const navHead = document.querySelector('#header')
 
-
 const showNav = () => {
-    navBtn.classList.toggle('btn-rotate')
-    navList.classList.toggle('show-navigation-list')
-    navHead.classList.toggle('header-scale')
-
+	navBtn.classList.toggle('btn-rotate')
+	navList.classList.toggle('show-navigation-list')
+	// navHead.classList.toggle('header-scale')
 }
+
+const hideNav = (e) => {
+	if (e.target.classList.contains('hide-nav') === false) {
+		navList.classList.remove('show-navigation-list')
+		navBtn.classList.remove('btn-rotate')
+		// navHead.classList.remove('header-scale')
+	}
+}
+
+const scrollCheck = () => {
+	if (scrollY > 90) {
+		navHead.classList.add('header-scale')
+	} else {
+		navHead.classList.remove('header-scale')
+	}
+}
+
+document.addEventListener('click', hideNav)
 navBtn.addEventListener('click', showNav)
+
+document.addEventListener('scroll', scrollCheck)
